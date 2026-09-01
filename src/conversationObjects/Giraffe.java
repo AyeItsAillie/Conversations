50ea3011077a44236b38b4a053e509a15f2a60d9
rblaha-dunwoody <rblaha@dunwoody.edu>
2026-09-01 13:42:11 -0500

    Adding Giraffe

---
+++ src/conversationObjects/Giraffe.java
@@ -0,0 +1,11 @@
+package conversationObjects;
+
+public class Giraffe extends Animal {
+    public Giraffe(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        say("weird mooing noises");
+    }
+}
